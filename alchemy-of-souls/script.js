0a01982f63559b986de4d82eb728cc0ef07bd445
sarahnguyen126 <sn24@pdx.edu>
2023-05-09 08:56:16 -0700

    added in poison drag

---
+++ alchemy-of-souls/script.js
@@ -0,0 +1,58 @@
+let dragged;
+
+/* events fired on the draggable target */
+const source = document.getElementById("poison-bottle-container");
+source.addEventListener("drag", (event) => {
+  console.log("dragging");
+});
+
+source.addEventListener("dragstart", (event) => {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.classList.add("dragging");
+});
+
+source.addEventListener("dragend", (event) => {
+  // reset the transparency
+  event.target.classList.remove("dragging");
+});
+
+/* events fired on the drop targets */
+const target = document.getElementById("ju-poison");
+target.addEventListener(
+  "dragover",
+  (event) => {
+    // prevent default to allow drop
+    event.preventDefault();
+    //add styles here
+  },
+  false
+);
+
+target.addEventListener("dragenter", (event) => {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.add("dragover");
+  }
+});
+
+target.addEventListener("dragleave", (event) => {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+  }
+});
+
+target.addEventListener("drop", (event) => {
+  let url = "poison-drinking.html";
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged element to the selected drop target
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+    event.target.appendChild(dragged);
+    alert("you have given Jang-Uk poison!!!");
+    window.location = url;
+  }
+});
